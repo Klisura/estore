62b9210b86103189ac170b0a102e12c1cf2a1060
Milan Trajkovic <lakocemo@outlook.com>
2024-10-15 22:43:47 -0400

    37

--- ecom/settings.py
+++ ecom/settings.py
@@ -21,8 +21,8 @@ SECRET_KEY = 'django-insecure-*0t^6v%rut$8y7ef+s7opel@!334-*ss^36g820hk$z$0+&k-m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['estore-production.up.railway.app', 'https://estore-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://estore-production.up.railway.app']
+ALLOWED_HOSTS = ['estore.railway.internal', 'https://estore.railway.internal']
+CSRF_TRUSTED_ORIGINS = ['estore.railway.internal']
 
 
 # Application definition
